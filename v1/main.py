b5f5de19eb81a6c384a9e3250f72c1f2ee305355
EC2 Default User <ec2-user@ip-172-31-35-207.us-east-2.compute.internal>
2018-02-04 02:41:21 +0000

    update1

--- v1/main.py
+++ v1/main.py
@@ -1,12 +1,14 @@
+import pandas as pd
 import extractor as ex
 titles = []
 abstracts = []
-authors = []
+authors_n = []
+authors_a = []
 date_ = [1802,1801,1712,1711,1710,1709,1708,1707,1706,1705,1704,1703,1702,1701]
 max_n = 15000
 len_max = 5
 for d in date_:
-	for i in range(1,max_n):
+	for i in range(400,max_n):
 	    n_zer = len_max-len(str(i))
 	    z= ''
 	    for j in range(1,n_zer):
@@ -15,11 +17,15 @@ for d in date_:
 	    html_doc = ex.read_page(name)  
 	    if "doesn't exist" in html_doc:
         	    break 
-	    titles.append(ex.extract_title(html_doc))
+	    title = ex.extract_title(html_doc)
+	    titles.append(title)
 	    abstracts.append(ex.extract_abstract(html_doc))
 	    address, names = ex.extract_authors(html_doc)
 	    authors_n.append(name)
 	    authors_a.append(address)
+	    print(name)
+	    if title is '':
+	    	print(html_doc)
 	se = pd.Series(titles)
 	se2 = pd.Series(abstracts)
 	se3 = pd.Series(authors_n)
